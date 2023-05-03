04112c712f61475a902e87ec271d5fd6f1e05ec3
Ezinwa charles <charleskelvin12@gmail.com>
2023-05-03 15:33:54 +0000

    asyn_comp

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@ import random
 import asyncio
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
     async_generator - function to loop 10 times
@@ -14,5 +15,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.unifrom(0, 10)
-
+        yield random.uniform(0, 10)

dadf7cdcd0223f20cf45b70a8f2a231ed69159db
Ezinwa charles <charleskelvin12@gmail.com>
2023-05-04 04:20:36 +0000

    async_comp

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" a python module to return numbers async comprehension"""
-from typing import list
+""" a python module to returns 10 random numbers using async comprehension"""
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension() -> list[float]:
+
+async def async_comprehension() -> List[float]:
     """
     async_comprehension- function to return 10 random numbers
     Arguments:
